ed698dddbfa413428ca90b9834760287feaa1794
Alef Moab de Souza <alefmsza@gmail.com>
2018-11-27 00:40:18 -0200

    Atividade finalizada com alerta da distância

--- LeafletJSExercise/map.js
+++ LeafletJSExercise/map.js
@@ -1,9 +1,11 @@
 let mymap = L.map('mapid');
+let latlngs = [];
 
 let getPosition = pos =>{
     let latitude = pos.coords.latitude;
     let longitude = pos.coords.longitude;
     let position = [latitude, longitude]
+    latlngs.push(position);
     return position;
 }
 
@@ -32,14 +34,13 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoibGl2ZWFsZWZhZG0iLCJhIjoiY2pvc3h3M3BiMHJkdTNxbWxidnB2bGp6ZSJ9.5zZFfdoOqKQVvmF6nbGmHw'
 }).addTo(mymap);
 
-let latlngs = [];
-let polyline = L.polyline(latlngs, {color: 'red'}).addTo(mymap);
-
 function onMapClick(e) {
-    latlong = L.GeoJSON.latLngToCoords(e.latlng);
-    latlngs.push(latlong);
+    latlngs.push([e.latlng.lat,e.latlng.lng]);
     let newMarker = L.marker(e.latlng).addTo(mymap);
+    let polyline = L.polyline(latlngs, {color: 'red'}).addTo(mymap);
+    alert(`Distância em Metros: ${e.latlng.distanceTo(latlngs[0])}`);
 
 }
+
 
 mymap.on('click', onMapClick);
